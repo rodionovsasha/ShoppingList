1f4ed9f56b0e84eb31d51013f4cd3c3e451947c2
rodionovsasha <allrod@mail.ru>
2016-10-25 07:54:22 +0100

    refactored cucumber steps with cucumber-java8

--- src/test/java/ru/rodionovsasha/shoppinglist/ui/steps/BaseSteps.java
+++ src/test/java/ru/rodionovsasha/shoppinglist/ui/steps/BaseSteps.java
@@ -1,9 +1,11 @@
 package ru.rodionovsasha.shoppinglist.ui.steps;
 
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import cucumber.api.java8.En;
 import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.rodionovsasha.shoppinglist.ui.BaseCucumberTest;
+import ru.rodionovsasha.shoppinglist.ui.utils.TestUtils;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
@@ -13,77 +15,57 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static org.junit.Assert.assertEquals;
 import static ru.rodionovsasha.shoppinglist.ui.utils.TestUtils.getTitle;
-import static ru.rodionovsasha.shoppinglist.ui.utils.TestUtils.openUrl;
 
 /*
  * Copyright (©) 2016. Rodionov Alexander
  */
 
-public class BaseSteps extends BaseCucumberTest {
-    @When("^I open page with url: (.*?)$")
-    public void openPageByUrl(String url) {
-        openUrl(url);
-    }
+public class BaseSteps extends BaseCucumberTest implements En {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseSteps.class);
 
-    @Then("^I should see All lists header$")
-    public void verifyOpenedAllListPage() throws Exception {
-        $("div.container h1").shouldHave(text("All lists"));
-    }
+    public BaseSteps() {
+        When("^I open page with url: (.*?)$", TestUtils::openUrl);
 
-    @Then("^I should see a page with title=(.*?)$")
-    public void verifyPageTitle(String title) {
-        assertEquals(title, getTitle());
-    }
+        When("^I should see All lists header$", () -> $("div.container h1").shouldHave(text("All lists")));
 
-    @Then("^I click on the link with text=(.*?)$")
-    public void clickOnTheLinkWithText(String text) {
-        $(By.linkText(text)).click();
-    }
+        Then("^I should see a page with title=(.*?)$", (String title) -> assertEquals(title, getTitle()));
 
-    @Then("^I click on the link with href=(.*?)$")
-    public void clickOnTheLinkWithHref(String href) {
-        $(By.xpath("//a[@href='" + href + "']")).click();
-    }
+        Then("^I click on the link with text=(.*?)$", (String text) -> $(By.linkText(text)).click());
 
-    @Then("^I should see link with text=(.*?)$")
-    public void verifyLinkWithText(String text) {
-        $(By.linkText(text)).should(exist);
-    }
+        Then("^I click on the link with href=(.*?)$", (String href) -> $(By.xpath("//a[@href='" + href + "']")).click());
 
-    @Then("^I should see link with href=(.*?)$")
-    public void verifyLinkWithUrl(String href) {
-        $(By.xpath("//a[@href=" + href + "]"));
-    }
+        Then("^I should see link with text=(.*?)$", (String text) -> $(By.linkText(text)).should(exist));
 
-    @Then("^I should see link with url=(.[^\\s]*?) and text=(.*?)$")
-    public void verifyLinkWithTextAndHref(String href, String text) throws UnsupportedEncodingException {
-        $(By.xpath("//a[contains(., '" + text + "') and contains(@href, '" + URLDecoder.decode(href, "UTF-8") + "')]")).should(exist);
-    }
+        Then("^I should see link with href=(.*?)$", (String href) -> $(By.xpath("//a[@href=" + href + "]")));
 
-    @Then("^I should see header with text=(.*?)$")
-    public void verifyHeader(String text) throws Exception {
-        $("h1").shouldHave(text(text));
-    }
+        Then("^I should see link with url=(.[^\\s]*?) and text=(.*?)$", (String href, String text) -> {
+            try {
+                $(By.xpath("//a[contains(., '" + text + "') and contains(@href, '" + URLDecoder.decode(href, "UTF-8") + "')]")).should(exist);
+            } catch (UnsupportedEncodingException e) {
+                LOGGER.warn("UnsupportedEncodingException:", e);
+            }
+        });
 
-    @Then("^I try to add new list with name=(.*?)$")
-    public void tryToAddNewList(String name) throws Exception {
-        $("#name").setValue(name);
-        $(By.name("addItemsList")).submit();
-    }
+        Then("^I should see header with text=(.*?)$", (String text) -> $("h1").shouldHave(text(text)));
 
-    @Then("^I try to update list with new name=(.*?)$")
-    public void tryToUpdateList(String newName) throws Exception {
-        $("#name").setValue(newName);
-        $(By.name("editedItemsList")).submit();
-    }
+        Then("^I try to add new list with name=(.*?)$", (String name) -> {
+            $("#name").setValue(name);
+            $(By.name("addItemsList")).submit();
+        });
 
-    @Then("^I should see error message=(.*?)$")
-    public void verifyErrorMessage(String message) throws Exception {
-        $("small.text-danger").shouldHave(text(message));
-    }
+        Then("^I try to update list with new name=(.*?)$", (String newName) -> {
+            $("#name").setValue(newName);
+            $(By.name("editedItemsList")).submit();
+        });
 
-    @Then("^I should not see link with url=(.[^\\s]*?)$")
-    public void verifyLinkWithTextAndHref(String href) throws UnsupportedEncodingException {
-        $(By.xpath("//a[contains(@href, '" + URLDecoder.decode(href, "UTF-8") + "')]")).shouldNot(exist);
+        Then("^I should see error message=(.*?)$", (String message) -> $("small.text-danger").shouldHave(text(message)));
+
+        Then("^I should not see link with url=(.[^\\s]*?)$", (String href) -> {
+            try {
+                $(By.xpath("//a[contains(@href, '" + URLDecoder.decode(href, "UTF-8") + "')]")).shouldNot(exist);
+            } catch (UnsupportedEncodingException e) {
+                LOGGER.warn("UnsupportedEncodingException:", e);
+            }
+        });
     }
 }
