7c9099787f64f978c268a838fa3b9c8b2cc43fd6
rodionovsasha <allrod@mail.ru>
2017-03-27 12:58:39 +0300

    updated ItemsListRestControllerTest

--- src/test/java/ru/rodionovsasha/shoppinglist/unit/controllers/rest/ItemsListRestControllerTest.java
+++ src/test/java/ru/rodionovsasha/shoppinglist/unit/controllers/rest/ItemsListRestControllerTest.java
@@ -4,10 +4,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ru.rodionovsasha.shoppinglist.controllers.rest.ItemsListRestController;
+import ru.rodionovsasha.shoppinglist.controllers.rest.RestExceptionHandlerController;
 import ru.rodionovsasha.shoppinglist.dto.ItemsListDto;
 import ru.rodionovsasha.shoppinglist.entities.ItemsList;
 import ru.rodionovsasha.shoppinglist.services.ItemsListService;
@@ -20,11 +20,12 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static ru.rodionovsasha.shoppinglist.Application.API_BASE_URL;
 import static ru.rodionovsasha.shoppinglist.TestApplicationConfiguration.*;
+import static ru.rodionovsasha.shoppinglist.TestUtils.asJsonString;
 import static ru.rodionovsasha.shoppinglist.controllers.ItemsListController.ITEMS_LIST_BASE_PATH;
 
 /*
@@ -42,7 +43,10 @@ public class ItemsListRestControllerTest {
     @Before
     public void setUp() {
         initMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(new ItemsListRestController(itemsListService)).build();
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(new ItemsListRestController(itemsListService))
+                .setControllerAdvice(new RestExceptionHandlerController())
+                .build();
         Mockito.reset(itemsListService);
         itemsList = new ItemsList();
         itemsList.setId(LIST_ID);
@@ -55,11 +59,12 @@ public class ItemsListRestControllerTest {
     public void shouldGetAllListsTest() throws Exception {
         when(itemsListService.findAllLists()).thenReturn(itemsLists);
         mockMvc.perform(get(API_BASE_URL))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(APPLICATION_JSON_UTF8))
                 .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$.id", is(1)))
-                .andExpect(jsonPath("$.name", is(LIST_NAME)));
+                .andExpect(jsonPath("$.[0].id", is(1)))
+                .andExpect(jsonPath("$.[0].name", is(LIST_NAME)));
         verify(itemsListService, times(1)).findAllLists();
         verifyNoMoreInteractions(itemsListService);
     }
@@ -79,76 +84,97 @@ public class ItemsListRestControllerTest {
     }
 
     @Test
-    public void shouldAddItemsListFormTest() throws Exception {
-        mockMvc.perform(get(ITEMS_LIST_BASE_PATH + "/add")) // .accept(APPLICATION_JSON_UTF8)
-                .andExpect(status().isOk())
-                .andExpect(view().name("addItemsList"));
-    }
-
-    @Test
     public void shouldSaveItemsListTest() throws Exception {
-        when(itemsListService.addItemsList(any(ItemsListDto.class))).thenReturn(LIST_ID);
-        mockMvc.perform(post(ITEMS_LIST_BASE_PATH + "/add")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .param("name", LIST_NAME))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl(ITEMS_LIST_BASE_PATH + "?id=" + LIST_ID));
+        mockMvc.perform(post(API_BASE_URL + ITEMS_LIST_BASE_PATH)
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(asJsonString(itemsList)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.*", hasSize(2)))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is(LIST_NAME)));
         verify(itemsListService, times(1)).addItemsList(any(ItemsListDto.class));
-        verifyNoMoreInteractions(itemsListService);
-    }
+        verifyNoMoreInteractions(itemsListService);}
 
     @Test
     public void shouldNotSaveItemsListIfHasErrorsTest() throws Exception {
-        mockMvc.perform(post(ITEMS_LIST_BASE_PATH + "/add")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
-                .andExpect(status().isOk())
-                .andExpect(view().name("addItemsList"));
+        itemsList.setName("");
+        mockMvc.perform(post(API_BASE_URL + ITEMS_LIST_BASE_PATH)
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(asJsonString(itemsList)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.*", hasSize(1)))
+                .andExpect(jsonPath("$.fieldErrors", hasSize(1)))
+                .andExpect(jsonPath("$.fieldErrors.[0].field", is("name")))
+                .andExpect(jsonPath("$.fieldErrors.[0].message", is("may not be empty")));
         verify(itemsListService, never()).addItemsList(any(ItemsListDto.class));
         verifyNoMoreInteractions(itemsListService);
     }
 
     @Test
-    public void shouldShowEditItemsListFormTest() throws Exception {
-        when(itemsListService.getItemsListById(LIST_ID)).thenReturn(itemsList);
-        mockMvc.perform(get(ITEMS_LIST_BASE_PATH + "/edit")
-                .param("id", LIST_ID_PARAM))
-                .andExpect(status().isOk())
-                .andExpect(view().name("editItemsList"))
-                .andExpect(model().attribute("itemsListDto", itemsList));
-        verify(itemsListService, times(1)).getItemsListById(LIST_ID);
-        verifyNoMoreInteractions(itemsListService);
-    }
-
-    @Test
     public void shouldSaveEditedItemsListTest() throws Exception {
-        mockMvc.perform(post(ITEMS_LIST_BASE_PATH + "/edit")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .param("id", LIST_ID_PARAM)
-                .param("name", ITEM_NAME))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl(ITEMS_LIST_BASE_PATH + "?id=" + LIST_ID));
+        itemsList.setName("new name");
+        when(itemsListService.getItemsListById(LIST_ID)).thenReturn(itemsList);
+
+        mockMvc.perform(put(API_BASE_URL + ITEMS_LIST_BASE_PATH)
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(asJsonString(itemsList)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.*", hasSize(2)))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("new name")));
+        verify(itemsListService, times(1)).getItemsListById(LIST_ID);
         verify(itemsListService, times(1)).updateItemsList(any(ItemsListDto.class));
         verifyNoMoreInteractions(itemsListService);
     }
 
     @Test
+    public void shouldNotUpdateNotExistingListTest() throws Exception {
+        itemsList.setId(100);
+        mockMvc.perform(put(API_BASE_URL + ITEMS_LIST_BASE_PATH)
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(asJsonString(itemsList)))
+                .andExpect(status().isNotFound());
+        verify(itemsListService, times(1)).getItemsListById(100);
+        verifyNoMoreInteractions(itemsListService);
+    }
+
+    @Test
     public void shouldNotSaveEditedItemsListIfHasErrorsTest() throws Exception {
-        mockMvc.perform(post(ITEMS_LIST_BASE_PATH + "/edit")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .param("id", LIST_ID_PARAM))
-                .andExpect(status().isOk())
-                .andExpect(view().name("editItemsList"));
+        itemsList.setName("");
+        mockMvc.perform(put(API_BASE_URL + ITEMS_LIST_BASE_PATH)
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(asJsonString(itemsList)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.*", hasSize(1)))
+                .andExpect(jsonPath("$.fieldErrors", hasSize(1)))
+                .andExpect(jsonPath("$.fieldErrors.[0].field", is("name")))
+                .andExpect(jsonPath("$.fieldErrors.[0].message", is("may not be empty")));
         verify(itemsListService, never()).updateItemsList(any(ItemsListDto.class));
         verifyNoMoreInteractions(itemsListService);
     }
 
     @Test
     public void shouldDeleteItemsListTest() throws Exception {
-        mockMvc.perform(get(ITEMS_LIST_BASE_PATH + "/delete")
-                .param("id", LIST_ID_PARAM))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/"));
+        when(itemsListService.getItemsListById(LIST_ID)).thenReturn(itemsList);
+        mockMvc.perform(delete(API_BASE_URL + ITEMS_LIST_BASE_PATH + "/{id}", LIST_ID))
+                .andExpect(status().isOk());
+        verify(itemsListService, times(1)).getItemsListById(LIST_ID);
         verify(itemsListService, times(1)).deleteItemsList(LIST_ID);
+        verifyNoMoreInteractions(itemsListService);
+    }
+
+    @Test
+    public void shouldNotDeleteNotExistingListTest() throws Exception {
+        itemsList.setId(100);
+        mockMvc.perform(delete(API_BASE_URL + ITEMS_LIST_BASE_PATH + "/{id}", 100)
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(asJsonString(itemsList)))
+                .andExpect(status().isNotFound());
+        verify(itemsListService, times(1)).getItemsListById(100);
         verifyNoMoreInteractions(itemsListService);
     }
 }
