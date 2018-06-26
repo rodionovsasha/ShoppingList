dfa8f0f55fccf23fd0f70dae4c2f7e5c041a256b
Rodionov Aleksandr <allrod@mail.ru>
2018-06-26 09:54:58 +0300

    update main.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,5 +1,7 @@
 $(function () {
-    $(document).ready(function () {
-        $('[data-toggle="tooltip"]').tooltip();
+  $(document).ready(function () {
+    $('[data-toggle="tooltip"]').on('click', function () {
+      $(this).tooltip('hide')
     });
+  });
 });
